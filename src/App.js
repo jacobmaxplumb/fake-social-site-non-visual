3a802b5b265acdf27582021443d021cbb2700949
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-10-04 19:46:50 -0400

    added fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState('first');
+  const [lastName, setLastName] = useState('last');
+  const [email, setEmail] = useState('email');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input value={firstName}/>
+        <input value={lastName}/>
+        <input value={email}/>
+      </form>
     </div>
   );
 }
