b066c5689b090e02bbb266cbc24c1380e9ed7b71
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-10-04 19:58:45 -0400

    updated project

--- src/App.js
+++ src/App.js
@@ -3,28 +3,38 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [firstName, setFirstName] = useState('first');
-  const [lastName, setLastName] = useState('last');
-  const [email, setEmail] = useState('email');
+  const [values, setValues] = useState({firstName: '', lastName: '', email: ''});
+  console.log(values);
+  // const [firstName, setFirstName] = useState('first');
+  // const [lastName, setLastName] = useState('last');
+  // const [email, setEmail] = useState('email');
 
-  const handleFirst = e => {
-    setFirstName(e.target.value);
+  const handleChanges = (e) => {
+    setValues({...values, [e.target.name]: e.target.value})
   }
+  // const handleFirst = e => {
+  //   setFirstName(e.target.value);
+  // }
 
-  const handleLast = (e) => {
-    setLastName(e.target.value);
-  }
+  // const handleLast = (e) => {
+  //   setLastName(e.target.value);
+  // }
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  }
+  // const handleEmail = (e) => {
+  //   setEmail(e.target.value);
+  // }
 
   return (
     <div className="App">
-      <form>
-        <input onChange={handleFirst} value={firstName}/>
+      {/* <form>
+        <input name="firstName" onChange={handleFirst} value={firstName}/>
         <input onChange={handleLast} value={lastName}/>
         <input onChange={handleEmail} value={email}/>
+      </form> */}
+      <form>
+        <input name="firstName" onChange={handleChanges} value={values.firstName}/>
+        <input name="lastName" onChange={handleChanges} value={values.lastName}/>
+        <input name="email" onChange={handleChanges} value={values.email}/>
       </form>
     </div>
   );
