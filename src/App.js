d7bcc7c6761702103402982564c42a9bb4a2fe3e
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-10-04 20:14:04 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import RandomForm from './components/RandomForm';
 
 function App() {
-  const [values, setValues] = useState({firstName: '', lastName: '', email: ''});
-  console.log(values);
-  // const [firstName, setFirstName] = useState('first');
-  // const [lastName, setLastName] = useState('last');
-  // const [email, setEmail] = useState('email');
-
-  const handleChanges = (e) => {
-    setValues({...values, [e.target.name]: e.target.value})
-  }
-  // const handleFirst = e => {
-  //   setFirstName(e.target.value);
-  // }
-
-  // const handleLast = (e) => {
-  //   setLastName(e.target.value);
-  // }
-
-  // const handleEmail = (e) => {
-  //   setEmail(e.target.value);
-  // }
 
   return (
     <div className="App">
-      {/* <form>
-        <input name="firstName" onChange={handleFirst} value={firstName}/>
-        <input onChange={handleLast} value={lastName}/>
-        <input onChange={handleEmail} value={email}/>
-      </form> */}
-      <form>
-        <input name="firstName" onChange={handleChanges} value={values.firstName}/>
-        <input name="lastName" onChange={handleChanges} value={values.lastName}/>
-        <input name="email" onChange={handleChanges} value={values.email}/>
-      </form>
+      <RandomForm />
     </div>
   );
 }
---
+++ src/components/RandomForm.js
@@ -0,0 +1,15 @@
+const RandomForm = (props) => {
+    const [values, setValues] = useState({ firstName: '', lastName: '', email: '' });
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+    return (
+        <form>
+            <input name="firstName" onChange={handleChanges} value={values.firstName} />
+            <input name="lastName" onChange={handleChanges} value={values.lastName} />
+            <input name="email" onChange={handleChanges} value={values.email} />
+        </form>
+    )
+}
+
+export default RandomForm;
